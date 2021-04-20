94b48e28429e6c598d3cb8af7c80d186a9caad5a
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2021-04-20 15:36:40 -0700

    first commit

---
+++ git.sh
@@ -0,0 +1,6 @@
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:jcmcnch/18S_sequencing_bias_determination_GA03_GP13.git
+git push -u origin main
